117ce038b3df5ac5b9ba8f52f5f0882e1e9f5ea2
Vetle <vetle@tjora.net>
2025-02-04 20:40:13 +0100

    fix: remove breaking bugs

--- log_csv.py
+++ log_csv.py
@@ -14,8 +14,8 @@ def save_and_exit(dataframe, indices):
    registered_df = df.loc[indices]
    df.drop(index=indices, inplace=True)
 
-   df.to_csv(f'{csv}_modified', index=False)
-   registered_df.to_csv(f'{csv}_registered', index=False)
+   df.to_csv(f'{csv[-4]}_modified.csv', index=False)
+   registered_df.to_csv(f'{csv[-4]}_registered.csv', index=False)
    sys.exit(2)
 
 # IMPROVEMENT: Make one max_length for 1st row and one for 2nd row to make spacing more natural.
@@ -24,6 +24,7 @@ def get_categories(conn, list_cat):
    If list_cat is false, simply returns a list of all categories. If list_cat is true, all the categories 
    will be printed in 3 separate columns as well. 
    '''
+   list_categories = 'SELECT category_name FROM categories ORDER BY category_name;'
    with conn.cursor() as cursor:
       cursor.execute(list_categories)
       categories = cursor.fetchall()
@@ -48,43 +49,59 @@ def get_categories(conn, list_cat):
          print(categories[-2] + space + categories[-1])
    return categories   
 
-def unregistered_product(row, conn, indices, df, empty_n=False):
+def unregistered_product(row, conn, indices, df, dt, empty_n=False):
+   insert_category = 'INSERT INTO categories (category_name) VALUES (%s);'
+   
+   check_product_existence = 'SELECT EXISTS (SELECT 1 FROM products WHERE product_name = %s);'
+
+   find_category_id = 'SELECT id FROM categories WHERE category_name = %s;'
+
+   insert_product = 'INSERT INTO products (product_name, category_id) VALUES (%s, %s);'
+
+   find_product_id = 'SELECT id FROM products WHERE product_name = %s;'
+
+   insert_product_purchase = '''
+      INSERT INTO purchases
+      (date, price, amount, product_id)
+      VALUES (%s, %s, %s, %s);
+   '''
+
    with conn.cursor() as cursor:
       user_input = input().lower()
       if user_input == 'n' or empty_n:
          print('Enter one of the existing categories or create a new one:')
-         categories = get_categories(list_cat=True)
+         categories = get_categories(conn, list_cat=True)
          new_input = input().lower()
          new_input = new_input[0].upper() + new_input[1:]
          if not new_input in categories:
             cursor.execute(insert_category, (new_input,))
-            connection.commit()
+            conn.commit()
          cursor.execute(find_category_id, (new_input,))
          category_id = cursor.fetchone()[0]
          cursor.execute(insert_product, (row['Product'], category_id))
-         connection.commit()
+         conn.commit()
          cursor.execute(find_product_id, (row['Product'],))
          product_id = cursor.fetchone()[0]
-         cursor.execute(insert_product_purchase, (date_time, row['Price'], row['Amount'], product_id))
-         connection.commit()
+         cursor.execute(insert_product_purchase, (dt, row['Price'], row['Amount'], product_id))
+         conn.commit()
          return True
       elif user_input[:2] == 'n ':
          if len(user_input) == 2:
-            return unregistered_product(row, conn, indices, df, empty_n=True)
+            return unregistered_product(row, indices, conn, df, dt, empty_n=True)
          category = user_input[2].upper() + user_input[3:]
-         categories = get_categories(list_cat=False)
+         categories = get_categories(conn, list_cat=False)
          # should be made a function (because repetition):
          if not category in categories:
             cursor.execute(insert_category, (category,))
-            connection.commit()
+            conn.commit()
          cursor.execute(find_category_id, (category,))
          category_id = cursor.fetchone()[0]
          cursor.execute(insert_product, (row['Product'], category_id))
-         connection.commit()
+         conn.commit()
          cursor.execute(find_product_id, (row['Product'],))
          product_id = cursor.fetchone()[0]
-         cursor.execute(insert_product_purchase, (date_time, row['Price'], row['Amount'], product_id))
-         connection.commit()
+         cursor.execute(insert_product_purchase, (dt, row['Price'], row['Amount'], product_id))
+         conn.commit()
          return True
          # end of "function".
       elif user_input == 's':
@@ -98,21 +115,21 @@ def unregistered_product(row, conn, indices, df, empty_n=False):
          cursor.execute(check_product_existence, (new_name,))
          if not cursor.fetchone()[0]:
             row['Product'] = new_name
-            print(f'Choose [n] to add {new_name} to a category.')
-            return unregistered_product(row, conn, indices, df)
+            print(f'Choose a category [n] to add {new_name} to a category.')
+            return unregistered_product(row, conn, indices, df, dt)
          else:
             cursor.execute(find_product_id, (new_name,))
             product_id = cursor.fetchone()[0]
-            cursor.execute(insert_product_purchase, (date_time, row['Price'], row['Amount'], product_id))
-            connection.commit()
+            cursor.execute(insert_product_purchase, (dt, row['Price'], row['Amount'], product_id))
+            conn.commit()
             return True
       elif user_input == 'd':
          return True
       elif user_input in ('e', 'exit', 'exit()'):
-         exit_and_save(df, indices)
+         save_and_exit(df, indices)
       else:
          print('Please enter a valid input.')
-         return unregistered_product(row, conn, indices, df)
+         return unregistered_product(row, indices, conn, df, dt)
 
 def main():
    # get csv name
@@ -142,23 +159,16 @@ def main():
    cursor = connection.cursor()
 
    # PSQL commands:
+
+   #check_category_existence = 'SELECT EXISTS (SELECT 1 FROM categories WHERE category_name = %s);'
+
+   #find_ids = 'SELECT id, category_id FROM products WHERE product_name = %s;'
+
+   #check_existing_purchases = 'SELECT 1 FROM purchases WHERE date = %s LIMIT 1;'
+
    check_product_existence = 'SELECT EXISTS (SELECT 1 FROM products WHERE product_name = %s);'
 
-   check_category_existence = 'SELECT EXISTS (SELECT 1 FROM categories WHERE category_name = %s);'
-
-   insert_category = 'INSERT INTO categories (category_name) VALUES (%s);'
-
-   insert_product = 'INSERT INTO products (product_name, category_id) VALUES (%s, %s);'
-
-   find_ids = 'SELECT id, category_id FROM products WHERE product_name = %s;'
-
-   find_category_id = 'SELECT id FROM categories WHERE category_name = %s;'
-
    find_product_id = 'SELECT id FROM products WHERE product_name = %s;'
-
-   check_existing_purchases = 'SELECT 1 FROM purchases WHERE date = %s LIMIT 1;'
-
-   list_categories = 'SELECT category_name FROM categories ORDER BY category_name;'
 
    insert_product_purchase = '''
       INSERT INTO purchases
@@ -178,7 +188,7 @@ def main():
          indices.append(index)
       else:
          print(f'"{row['Product']}" not found. Please register new entry (n), modify product name (m), skip this product (s), delete this product (d) or exit (e).')
-         if unregistered_product(row, indices, connection, df):
+         if unregistered_product(row, connection, indices, df, date_time):
             indices.append(index)
    registered_df = df.loc[indices]
    df.drop(index=indices, inplace=True)
@@ -186,8 +196,8 @@ def main():
    if df.empty:
       sys.exit(0)
    else:
-      df.to_csv(f'{csv}_modified', index=False)
-      registered_df.to_csv(f'{csv}_registered', index=False)
+      df.to_csv(f'{csv[-4]}_modified.csv', index=False)
+      registered_df.to_csv(f'{csv[:-4]}_registered.csv', index=False)
       sys.exit(2)
 
 if __name__ == '__main__':
