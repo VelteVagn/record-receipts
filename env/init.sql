3bf464cbb6ae85e74cb3ea7a2e3fe79641298890
Vetle <vetle@tjora.net>
2025-02-19 14:45:07 +0100

    docs: create init.sql

---
+++ env/init.sql
@@ -0,0 +1,27 @@
+-- create database
+CREATE DATABASE receipts;
+
+-- switch to the database
+\c receipts;
+
+-- create table for categories
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    category_name TEXT UNIQUE NOT NULL
+);
+
+-- create table for products
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    product_name TEXT UNIQUE NOT NULL,
+    category_id INT references categories(id) NOT NULL
+);
+
+-- create table for purchases
+CREATE TABLE purchases (
+    id SERIAL PRIMARY KEY,
+    date TIMESTAMP NOT NULL,
+    price NUMERIC(10,2) NOT NULL,
+    amount INT NOT NULL,
+    product_id INT references products(id) NOT NULL
+);
